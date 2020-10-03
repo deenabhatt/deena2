e82b2e5abdf22885ad1302d72debe30ab1dd8bee
Deena Bhatt <deenabhatt1@gmail.com>
2020-10-02 23:56:44 -0500

    second commit test

---
+++ process.py
@@ -0,0 +1,12 @@
+log_file = open("um-server-01.txt")
+
+
+def sales_reports(log_file): # This line will creat the function with parameter log file
+    for line in log_file:    # for loop will cycle through each line in the para meter log file.
+        line = line.rstrip() # this will remove all speces 
+        day = line[0:3]      # This code will store fist three characters of line in variable'day'
+        if day == "sat":     # this line will just itrate mon
+            print(line)
+
+
+sales_reports(log_file)
