53843b51d97978e4f79b19277d32f7b5a4fdb88a
Deena Bhatt <deenabhatt1@gmail.com>
2020-10-03 12:02:07 -0500

    testing a changed for push to Repo

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ def sales_reports(log_file): # This line will creat the function with parameter 
     for line in log_file:    # for loop will cycle through each line in the para meter log file.
         line = line.rstrip() # this will remove all speces 
         day = line[0:3]      # This code will store fist three characters of line in variable'day'
-        if day == "sat":     # this line will just itrate mon
+        if day == "Fri":     # this line will just itrate mon
             print(line)
 
 
